7fb75095530e7d2fece92640ff01a59b213cb01f
Vinod Muda <vinod.muda@gmail.com>
2018-03-04 21:31:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sepick-core
---
+++ src/main/java/AppTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Vinod on 3/1/2018.
+ */
+public class AppTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
