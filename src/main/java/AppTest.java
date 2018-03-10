97e933110a99c15145ca1db991f65938e67808d5
Vinod Muda <vinod.muda@gmail.com>
2018-03-10 19:37:11 +0530

    Added Test

--- src/main/java/AppTest.java
+++ src/main/java/AppTest.java
@@ -1,8 +1,13 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Vinod on 3/1/2018.
  */
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloBean");
+        helloWorld.printHello();
     }
 }
