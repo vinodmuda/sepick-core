6c4411e5e4daeb1c4c0b10d3bb0091399b6432de
Vinod Muda <vinod.muda@gmail.com>
2018-03-19 18:33:04 +0530

    Renamed Sepick constants file

--- src/main/java/com/sepick/core/SepickCoreConstants.java
+++ src/main/java/com/sepick/core/SepickCoreConstants.java
@@ -3,7 +3,7 @@ package com.sepick.core;
 /**
  * Created by Vinod on 3/10/2018.
  */
-public class SepickConstants {
+public class SepickCoreConstants {
 
     public static final String SEPICK_APP_CONTEXT_FILE = "applicationContext.xml";
     public static final String SEPICK_PROPERTIES_FILE = "sepick-core.properties";
